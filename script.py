f6dbbba82dcf768d3f95a2c59c644714a4ef54a2
A-Schmigiel <avianschmigiel@gmail.com>
2024-08-28 14:41:37 +0100

    Originating files and committing

---
+++ script.py
@@ -0,0 +1,18 @@
+# Towers of Hanoi game sim.
+# Goal: move 4 stacked disks from leftmost of 3 rods to the rightmost.
+# rules: 1) only one disk may be moved at a time. 2)Each move consists of taking
+from stack import Stack
+
+print("\nLet's play Towers of Hanoi!!")
+
+#Create the Stacks
+
+
+
+#Set up the Game
+
+
+#Get User Input
+
+
+#Play the Game
